47247d9550a9a760e2334681da83530ffc015a2e
raghav9k <raghav.khurana9999@gmail.com>
2022-03-08 00:29:05 -0800

    basic html

--- my_project/boards/views.py
+++ my_project/boards/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from .models import Board
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+
+    return render(request, 'home.html', {'boards':boards})
